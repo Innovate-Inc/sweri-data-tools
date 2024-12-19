f8647ec6c5cc456f25d5a98521bfd724696f1cf3
Keaton Shennan <kshennan@innovateteam.com>
2024-12-19 09:14:41 -0700

    in progress docker

--- intersections-postgis.py
+++ intersections-postgis.py
@@ -1,13 +1,14 @@
 import os
 
 from dotenv import load_dotenv
+from osgeo import ogr
 
+from sweri_utils.download import fetch_features
 from sweri_utils.sql import connect_to_pg_db
 
 if __name__ == '__main__':
     # Define the gdb and sde and load .env
     load_dotenv()
-
     wkid = 3857
 
     schema = os.getenv('SCHEMA')
@@ -19,5 +20,12 @@ if __name__ == '__main__':
         os.getenv('DB_USER'),
         os.getenv('DB_PASSWORD')
     )
+
+    print(help(ogr))
+    # create the template for the new intersect
+    treatment_intersections = os.path.join(os.getenv('SDE_FILE'), f'sweri.{schema}.intersections')
+    # fetch_features()
+    print(pg_cursor.execute('select * from information_schema.tables'))
+    print('did something')
      # fetch all features and create featureclass from intersection features
     
