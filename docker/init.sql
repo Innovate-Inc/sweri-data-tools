74495206bf7b6b3cad97a2fa3d7dfaa20f44ebcb
Keaton Shennan <kshennan@innovateteam.com>
2025-01-21 17:08:30 -0700

    in progress updates

--- docker/init.sql
+++ docker/init.sql
@@ -1,4 +1,13 @@
+BEGIN;
 CREATE USER sweri;
-CREATE DATABASE sweri;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'sweri') THEN
+        CREATE DATABASE sweri;
+    END IF;
+END $$;
+\c sweri
 CREATE SCHEMA sweri;
+CREATE EXTENSION postgis;
 GRANT ALL PRIVILEGES ON DATABASE sweri TO sweri;
+COMMIT;
--- intersections-postgis.py
+++ intersections-postgis.py
@@ -1,8 +1,14 @@
+import logging
 import os
-import  requests as r
+import requests as r
 from dotenv import load_dotenv
-# from osgeo import ogr
-from sweri_utils.sql import connect_to_pg_db
+from osgeo import ogr
+from sweri_utils.sql import connect_to_pg_db, rename_postgres_table
+import sys
+# import watchtower
+logger = logging.getLogger(__name__)
+logging.basicConfig( format='%(asctime)s %(levelname)-8s %(message)s', encoding='utf-8', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+# logger.addHandler(watchtower.CloudWatchLogHandler())
 
 def get_intersection_features(url, layer_id=0):
     intersection_r = r.get(url + f'/{layer_id}/query',
@@ -26,7 +32,6 @@ def configure_intersection_sources(features, coded_vals):
             intersect_targets[att['id_source']] = s
     return intersect_sources, intersect_targets
 
-
 def query_coded_value_domain(url, layer):
     domain_r = r.get(url + f'/queryDomains',
                       params={'f': 'json', 'layers': layer})
@@ -38,31 +43,102 @@ def query_coded_value_domain(url, layer):
     cv = d_json['domains'][0]['codedValues']
     return {c['code']: c['name'] for c in cv}
 
+def calculate_intersections_and_insert(cursor, schema, insert_table, source_key, target_key):
+    logger.info(f'beginning intersections on {source_key} and {target_key}')
+    query = f""" insert into {schema}.{insert_table} (acre_overlap, id_1, id_1_source, id_2, id_2_source)
+         select ST_AREA(ST_TRANSFORM(ST_INTERSECTION(a.shape, b.shape),4326)::geography) * 0.000247105 as acre_overlap, 
+         a.unique_id as id_1, 
+         a.feat_source as id_1_source, 
+         b.unique_id as id_2, 
+         b.feat_source as id_2_source
+         from sweri.intersection_features a, sweri.intersection_features b
+         where ST_INTERSECTS (a.shape, b.shape) 
+         and a.feat_source = '{source_key}'
+         and b.feat_source = '{target_key}';"""
+    cursor.execute('BEGIN;')
+    cursor.execute(query)
+    cursor.execute('COMMIT;')
+    logger.info(f'completed intersections on {source_key} and {target_key}, inserted into {schema}.{insert_table} ')
+
+def calculate_intersections_from_sources(intersect_sources, intersect_targets, new_intersections_name, cursor, schema):
+
+    for source_key, source_value in intersect_sources.items():
+        for target_key, target_value in intersect_targets.items():
+            if target_key == source_key:
+                continue
+            logger.info(f'performing intersections on {source_key} and {target_key}')
+            calculate_intersections_and_insert(cursor, schema, new_intersections_name, source_key, target_key)
+            logger.info(f'completed intersections on {source_key} and {target_key}')
+
+def swap_intersection_tables(cursor, schema):
+    """
+    swaps new intersections and existing intersections table
+    :param connection: ArcSDESQLExecute connection
+    :param schema: target schema
+    :return:
+    """
+    logger.info('moving to postgres table updates')
+    # rename backup backup to temp table to make space for new backup
+    rename_postgres_table(cursor, schema, 'intersections_backup',
+                          'intersections_backup_temp')
+    logger.info(
+        f'{schema}.intersections_backup renamed to {schema}.intersections_backup_temp')
+
+    # rename current table to backup table
+    rename_postgres_table(cursor, schema, 'intersections', 'intersections_backup')
+    logger.info(f'{schema}.intersections renamed to {schema}.intersections_backup')
+    # rename new intersections table to new data
+    rename_postgres_table(cursor, schema, 'new_intersections', 'intersections')
+    logger.info(f'{schema}.new_intersections renamed to {schema}.intersections')
+
+    # drop temp backup table
+    cursor.execute(f'DROP TABLE IF EXISTS {schema}.intersections_backup_temp CASCADE;')
+    logger.info(f'{schema}.intersections_backup_temp deleted')
+
+def geojson_to_postgis(json, postgis_connection, table):
+
+    pass
+
+def org_connect_to_postgis(connection_str):
+    return ogr.GetDriver('PostgreSQL').createDataSource(connection_str)
+
 if __name__ == '__main__':
+    print('hello again')
+    sys.exit(1)
     # Define the gdb and sde and load .env
     load_dotenv()
     wkid = 3857
+    # get db schema
+    db_schema = os.getenv('SCHEMA')
+    db_host = os.getenv('DB_HOST'),
+    db_port = os.getenv('DB_PORT'),
+    db_name = os.getenv('DB_NAME'),
+    db_user = os.getenv('DB_USER'),
+    db_pw = os.getenv('DB_PASSWORD')
+    # psycopg2 connection, because arcsde connection is extremely slow during inserts
+    pg_cursor = connect_to_pg_db(
+        db_host, db_port, db_name, db_user, db_pw
+    )
+
+    # configure intersection sources
     intersection_src_url = os.getenv('INTERSECTION_SOURCES_URL')
     # setup intersection source features
+    ############## setting intersection sources ################
     intersections = get_intersection_features(intersection_src_url)
     # handle coded value domains
     cvs = query_coded_value_domain(intersection_src_url, 0)
-    # configure intersection sources
     sources = configure_intersection_sources(intersections, cvs)
-    # get db schema
-    schema = os.getenv('SCHEMA')
-    # psycopg2 connection, because arcsde connection is extremely slow during inserts
-    pg_cursor = connect_to_pg_db(
-        os.getenv('DB_HOST'),
-        os.getenv('DB_PORT'),
-        os.getenv('DB_NAME'),
-        os.getenv('DB_USER'),
-        os.getenv('DB_PASSWORD')
-    )
+    intersect_sources = sources[0]
+    intersect_targets = sources[1]
+
+    ############## updating data ################
+    # iterate over intersection sources
+    postgis_conn_str = f"PG:host={db_host} dbname={db_name} user={db_user} password={db_pw}"
+    ogr_pg_conn = ogr_connect_to_postgis(postgis_conn_str)
+    ############## calculating intersections ################
+    calculate_intersections_from_sources(intersect_sources, intersect_targets, 'new_intersections', pg_cursor,
+                                         schema)
 
     # create the template for the new intersect
-    treatment_intersections = os.path.join(os.getenv('SDE_FILE'), f'sweri.{schema}.intersections')
-    print(pg_cursor.execute('select * from information_schema.tables'))
-    print('did something')
-     # fetch all features and create featureclass from intersection features
-    
+    swap_intersection_tables(pg_cursor, schema)
+    logger.info(f'INTERSECTIONS UPDATES COMPLETE')
