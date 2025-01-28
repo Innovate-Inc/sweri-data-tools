4952f05556c61cec5dabb04b767981c6696e2233
Keaton Shennan <kshennan@innovateteam.com>
2025-01-28 16:17:31 -0700

    update docker compose volume mapping for pgadmin, add methods for updating intersection source last_run, insert geojson features into db

--- intersections-postgis.py
+++ intersections-postgis.py
@@ -44,22 +44,40 @@ def configure_intersection_sources(features, coded_vals, start):
             s['name'] = coded_vals[att['name']]
 
         att = att
+        # always set targets
+        if att['use_as_target'] == 1:
+            intersection_targets[att['id_source']] = s
         if att['last_run'] is not None and (start - datetime.fromtimestamp(att['last_run']/1000)).days < att['frequency_days']:
             logger.info(f'skipping {s["name"] if s["name"] else att["id_source"]}, last run less than frequency')
             continue
 
         intersection_sources[att['id_source']] = s
-        if att['use_as_target'] == 1:
-            intersection_targets[att['id_source']] = s
+
     return intersection_sources, intersection_targets
 
-def update_last_run(features, start_time, url):
+def update_last_run(features, start_time, url, layer_id):
     '''
     updates the last run date for the intersection source, run after intersections complete
     :param features:
     :return:
     '''
-    pass
+    update_feat = json.dumps([
+        {
+            'attributes': {
+                'objectid': f['attributes']['objectid'],
+                'last_run': start_time.timestamp() *1000
+            }
+        } for f in features
+    ])
+
+
+    update_r = r.post(url + f'/{layer_id}/updateFeatures', params={'f': 'json', 'features': update_feat})
+    if 'updateResults' not in update_r.json():
+        raise ValueError('update failed: missing update results')
+    errors = [e for e in update_r.json()['updateResults'] if e['success'] is False]
+    if len(errors) > 0:
+        raise ValueError(f'update failed: {errors}')
+    logger.info('completed last run update')
 
 
 def query_coded_value_domain(url, layer):
@@ -113,12 +131,12 @@ def swap_intersection_tables(cursor, schema):
     # rename backup backup to temp table to make space for new backup
     rename_postgres_table(cursor, schema, 'intersections_backup',
                           'intersections_backup_temp')
-    logger.info(
-        f'{schema}.intersections_backup renamed to {schema}.intersections_backup_temp')
+    logger.info(f'{schema}.intersections_backup renamed to {schema}.intersections_backup_temp')
 
     # rename current table to backup table
     rename_postgres_table(cursor, schema, 'intersections', 'intersections_backup')
     logger.info(f'{schema}.intersections renamed to {schema}.intersections_backup')
+
     # rename new intersections table to new data
     rename_postgres_table(cursor, schema, 'new_intersections', 'intersections')
     logger.info(f'{schema}.new_intersections renamed to {schema}.intersections')
@@ -127,7 +145,7 @@ def swap_intersection_tables(cursor, schema):
     cursor.execute(f'DROP TABLE IF EXISTS {schema}.intersections_backup_temp CASCADE;')
     logger.info(f'{schema}.intersections_backup_temp deleted')
 
-def fetch_features_and_create_geojson(service_url, where, fc_name, geometry=None, geom_type=None, out_sr=3857,
+def fetch_features_and_dump_geojson(service_url, where, geometry=None, geom_type=None, out_sr=4326,
                                       out_fields=None, chunk_size=2000):
     ids = get_ids(service_url, where, geometry, geom_type)
     out_features = []
@@ -135,12 +153,22 @@ def fetch_features_and_create_geojson(service_url, where, fc_name, geometry=None
     for f in get_all_features(service_url, ids, out_sr, out_fields, chunk_size, 'geojson'):
         # change to insert directly into db
         out_features += f
+
     if len(out_features) == 0:
         raise Exception(f'No features fetched for ids: {ids}')
-    geojson_feat = {'type': 'FeatureCollection', 'features': out_features}
-    with open(f'{fc_name}.geojson', 'w') as f:
-        json.dump(geojson_feat, f)
-    return f
+    return out_features
+
+def insert_feature_into_db(cursor, target_table, feature, fc_name, id_field):
+    if ('geometry' not in feature) or ('properties' not in feature):
+        raise KeyError('missing geometry or properties')
+    if id_field not in feature['properties']:
+        raise KeyError(f'missing or incorrect id field: {id_field}')
+    logger.info(f'inserting feature into {fc_name}')
+    json_geom = json.dumps(feature['geometry'])
+    q = f"INSERT INTO {target_table} (unique_id, feat_source, shape) VALUES ('{feature['properties'][id_field]}', '{fc_name}',ST_SetSRID(ST_GeomFromGeoJSON('{json_geom}'), 4326));"
+    cursor.execute('BEGIN;')
+    cursor.execute(q)
+    cursor.execute('COMMIT;')
 
 
 
@@ -172,11 +200,12 @@ def delete_intersection_features(cursor, schema, source):
     logging.info(f'deleted feat_source: {source} from {schema}.intersection_features')
 
 
-def fetch_all_features_to_intersect(intersect_sources, pg_cursor, schema, insert_table='intersection_features',
-                                    wkid=3857):
+def fetch_all_features_to_intersect(intersect_sources, pg_cursor, schema, insert_table, wkid):
     for key, value in intersect_sources.items():
         if value['source_type'] == 'url':
-            fetch_features_and_create_geojson(value['source'],'1=1', key)
+            out_feat = fetch_features_and_dump_geojson(value['source'],'1=1', None, None, wkid)
+            for f in out_feat:
+                insert_feature_into_db(pg_cursor, f'{schema}.{insert_table}', f, key, value['id'])
         elif value['source_type'] == 'db_table':
             insert_from_db(pg_cursor, schema, insert_table,
                            ( 'shape', 'unique_id', 'feat_source'), value['source'],
@@ -190,7 +219,7 @@ if __name__ == '__main__':
     load_dotenv('.env')
     print('hello again')
     script_start = datetime.now()
-    wkid = 3857
+    wkid = 4326
     # get db schema
     db_schema = os.getenv('SCHEMA')
     db_host = os.getenv('DB_HOST')
@@ -213,9 +242,6 @@ if __name__ == '__main__':
     intersect_sources = sources[0]
     intersect_targets = sources[1]
 
-    print(intersect_sources)
-    print(intersect_targets)
-
     ############## setting up intersection features ################
     # setup intersection features table
     configure_intersection_features_table(pg_cursor, db_schema)
@@ -224,22 +250,12 @@ if __name__ == '__main__':
         delete_intersection_features(pg_cursor, db_schema, src)
     ############## fetching features ################
     # get latest features based on source
+    fetch_all_features_to_intersect(intersect_sources, pg_cursor, db_schema, 'intersection_features', wkid)
 
-    ############## updating data ################
-    # iterate over intersection sources
-    postgis_conn_str = f"PG:host='{db_host}' port='{db_port}' dbname='{db_name}' user='{db_user}' password='{db_pw}' active_schema='{db_schema}'"
-    ogr.RegisterAll()
-    ogr_conn = ogr.Open(postgis_conn_str)
-    new_int = ogr_conn.GetLayer('new_intersections')
-
-
-
-    # Close connection
-    conn = None
     ############## calculating intersections ################
-    # calculate_intersections_from_sources(intersect_sources, intersect_targets, 'new_intersections', pg_cursor,
-    #                                      schema
+    calculate_intersections_from_sources(intersect_sources, intersect_targets, 'new_intersections', pg_cursor, db_schema)
     # create the template for the new intersect
-    # swap_intersection_tables(pg_cursor, schema)
+    swap_intersection_tables(pg_cursor, db_schema)
     ############## update run info on intersection sources table ################
-    update_last_run(intersections, script_start, intersection_src_url)
+    update_last_run(intersections, script_start, intersection_src_url, 0)
+    logger.info('completed intersection processing')
