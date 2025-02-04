40b48e21b6ba6c7fa329fac48bde538d93f4ddc5
Keaton Shennan <kshennan@innovateteam.com>
2025-02-04 08:20:44 -0700

    fix ordering, add function for regenerating spatial index and running vacuum analyze

--- intersections.py
+++ intersections.py
@@ -12,12 +12,13 @@ from sweri_utils.s3 import upload_to_s3
 from sweri_utils.sql import connect_to_pg_db, rename_postgres_table, insert_from_db, pg_copy_to_csv
 
 import watchtower
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', encoding='utf-8', level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
-
 logger.addHandler(watchtower.CloudWatchLogHandler())
+
 
 def get_intersection_features(url, layer_id=0):
     intersection_r = r.get(url + f'/{layer_id}/query',
@@ -48,13 +49,15 @@ def configure_intersection_sources(features, coded_vals, start):
         # always set targets
         if att['use_as_target'] == 1:
             intersection_targets[att['id_source']] = s
-        if att['last_run'] is not None and (start - datetime.fromtimestamp(att['last_run']/1000)).days < att['frequency_days']:
+        if att['last_run'] is not None and (start - datetime.fromtimestamp(att['last_run'] / 1000)).days < att[
+            'frequency_days']:
             logger.info(f'skipping {s["name"] if s["name"] else att["id_source"]}, last run less than frequency')
             continue
 
         intersection_sources[att['id_source']] = s
 
     return intersection_sources, intersection_targets
+
 
 def update_last_run(features, start_time, url, layer_id):
     """
@@ -74,7 +77,7 @@ def update_last_run(features, start_time, url, layer_id):
         {
             'attributes': {
                 'objectid': f['attributes']['objectid'],
-                'last_run': start_time.timestamp() *1000
+                'last_run': start_time.timestamp() * 1000
             }
         } for f in features
     ])
@@ -153,8 +156,9 @@ def swap_intersection_tables(cursor, schema):
     cursor.execute(f'DROP TABLE IF EXISTS {schema}.intersections_backup_temp CASCADE;')
     logger.info(f'{schema}.intersections_backup_temp deleted')
 
+
 def fetch_features_and_dump_geojson(service_url, where, geometry=None, geom_type=None, out_sr=4326,
-                                      out_fields=None, chunk_size=100):
+                                    out_fields=None, chunk_size=100):
     ids = get_ids(service_url, where, geometry, geom_type)
     out_features = []
     # get all features
@@ -166,12 +170,13 @@ def fetch_features_and_dump_geojson(service_url, where, geometry=None, geom_type
         raise Exception(f'No features fetched for ids: {ids}')
     return out_features
 
+
 def insert_feature_into_db(cursor, target_table, feature, fc_name, id_field):
     if ('geometry' not in feature) or ('properties' not in feature):
         raise KeyError('missing geometry or properties')
     if id_field not in feature['properties']:
         raise KeyError(f'missing or incorrect id field: {id_field}')
-    
+
     json_geom = json.dumps(feature['geometry'])
     q = f"INSERT INTO {target_table} (unique_id, feat_source, shape) VALUES ('{feature['properties'][id_field]}', '{fc_name}',ST_SetSRID(ST_GeomFromGeoJSON('{json_geom}'), 4326));"
     try:
@@ -181,7 +186,6 @@ def insert_feature_into_db(cursor, target_table, feature, fc_name, id_field):
     except Exception as e:
         logger.error(f'error inserting feature: {e}, {q}')
         cursor.execute('ROLLBACK;')
-
 
 
 def configure_intersection_features_table(cursor, schema):
@@ -199,8 +203,10 @@ def configure_intersection_features_table(cursor, schema):
     rename_postgres_table(cursor, schema, 'intersection_features', 'intersection_features_backup')
     logger.info(f'{schema}.intersection_features renamed to {schema}.intersection_features_backup')
     # rename new intersections table to new data
-    cursor.execute(f'CREATE TABLE {schema}.intersection_features AS SELECT * FROM {schema}.intersection_features_backup')
+    cursor.execute(
+        f'CREATE TABLE {schema}.intersection_features AS SELECT * FROM {schema}.intersection_features_backup')
     logger.info(f'created {schema}.intersection_features from {schema}.intersection_features_backup')
+
 
 def configure_new_intersections_table(cursor, schema):
     logger.info('moving to postgres table updates')
@@ -224,16 +230,29 @@ def delete_intersection_features(cursor, schema, source):
 def fetch_features_to_intersect(intersect_sources, cursor, schema, insert_table, wkid):
     for key, value in intersect_sources.items():
         if value['source_type'] == 'url':
-            out_feat = fetch_features_and_dump_geojson(value['source'],'SHAPE IS NOT NULL', None, None, wkid)
+            out_feat = fetch_features_and_dump_geojson(value['source'], 'SHAPE IS NOT NULL', None, None, wkid)
             for f in out_feat:
                 insert_feature_into_db(cursor, f'{schema}.{insert_table}', f, key, value['id'])
         elif value['source_type'] == 'db_table':
             insert_from_db(cursor, schema, insert_table,
                            ('unique_id', 'feat_source'), value['source'],
                            # do not need to specify object id as we are using sde.net_rowid() in the insert
-                           ( value['id'], f"'{key}'"))
+                           (value['id'], f"'{key}'"))
         else:
             raise ValueError('invalid source type: {}'.format(value['source_type']))
+
+
+def refresh_spatial_index_analyze(cursor, schema, table):
+    logger.info(f'refreshing spatial index on {schema}.{table}')
+    cursor.execute('BEGIN;')
+    cursor.execute(f'DROP INDEX IF EXISTS {table}_shape_idx;')
+    # recreate index
+    cursor.execute(f'CREATE INDEX {table}_shape_idx ON {schema}.{table} USING GIST (shape);')
+    # run VACUUM ANALYZE to increase performance after bulk updates
+    cursor.execute(f'VACUUM ANALYZE {schema}.{table};')
+    cursor.execute('COMMIT;')
+    logger.info(f'refreshed spatial index on {schema}.{table}')
+
 
 if __name__ == '__main__':
     logger.info('starting intersection processing')
@@ -267,22 +286,28 @@ if __name__ == '__main__':
     for src in intersect_sources.keys():
         delete_intersection_features(pg_cursor, db_schema, src)
 
+    ############## fetching features ################
+    # get latest features based on source
+    fetch_features_to_intersect(intersect_sources, pg_cursor, db_schema, 'intersection_features', wkid)
+    # refresh the spatial index
+    refresh_spatial_index_analyze(pg_cursor, db_schema, 'intersection_features')
+
     ############## calculating intersections ################
     # setup table
     configure_new_intersections_table(pg_cursor, db_schema)
     # calculate intersections
-    calculate_intersections_from_sources(intersect_sources, intersect_targets, 'new_intersections', pg_cursor, db_schema)
+    calculate_intersections_from_sources(intersect_sources, intersect_targets, 'new_intersections', pg_cursor,
+                                         db_schema)
     # create the template for the new intersect
     swap_intersection_tables(pg_cursor, db_schema)
 
     ############## update run info on intersection sources table ################
-    update_last_run(intersections, script_start, intersection_src_url, 0) ############## fetching features ################
-    # get latest features based on source
-    fetch_features_to_intersect(intersect_sources, pg_cursor, db_schema, 'intersection_features', wkid)
-
+    update_last_run(intersections, script_start, intersection_src_url, 0)
     ############## write to csv and upload to s3 ################
     logger.info('uploading csv to s3')
-    create_csv_and_upload_to_s3(pg_cursor, db_schema, 'intersections', f'intersections_{db_schema}.csv', os.getenv('S3_BUCKET'))
+    create_csv_and_upload_to_s3(pg_cursor, db_schema, 'intersections', f'intersections_{db_schema}.csv',
+                                os.getenv('S3_BUCKET'))
     logger.info('completed upload to s3')
     conn.close()
+
     logger.info('completed intersection processing')
