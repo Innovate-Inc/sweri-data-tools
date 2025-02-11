1d309598c9696f8229f32e9e0cd67d635b482b91
Keaton Shennan <kshennan@innovateteam.com>
2025-02-11 08:47:34 -0700

    remove file as argument in cursor.copy in copy to csv helper function

--- sweri_utils/sql.py
+++ sweri_utils/sql.py
@@ -83,8 +83,7 @@ def pg_copy_to_csv(cursor: psycopg.Cursor, schema: str, table: str, filename: st
     """
     with open(filename, 'w') as f:
         with cursor.copy(
-            f'COPY (SELECT row_number() OVER () AS objectid, {",".join(columns)} FROM {schema}.{table}) TO STDOUT WITH CSV HEADER',
-            f) as copy:
+            f'COPY (SELECT row_number() OVER () AS objectid, {",".join(columns)} FROM {schema}.{table}) TO STDOUT WITH CSV HEADER') as copy:
                 while data := copy.read():
                     f.write(data)
     return f
