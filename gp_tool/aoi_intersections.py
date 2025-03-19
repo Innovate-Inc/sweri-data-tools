75bd2a4b44e5bbbc452e94a560066c35a9b0bac3
Keaton Shennan <kshennan@innovateteam.com>
2025-03-19 08:37:55 -0600

    Revert "remove coded value domain"
    This reverts commit c0dc2a746800957ba4fc8d1ff5af14f593068b81.

--- gp_tool/aoi_intersections.py
+++ gp_tool/aoi_intersections.py
@@ -1,5 +1,3 @@
-import json
-
 import arcpy
 from os import path
 import sys, os
@@ -15,71 +13,15 @@ from arcpy.management import CreateTable
 #                         stderrToServer=True)
 #########################################################map
 
-def configure_intersection_sources(sde_connection_file, schema):
-    """
-    configures intersection sources fetched from intersection source list
-    :param sde_connection_file: sde connection file path
-    :param schema: target schema
-    :return: intersection sources and intersection targets dictionaries
-    """
-    intersect_sources = {}
-    intersect_targets = {}
-    domains = fetch_domains(sde_connection_file,
-                            path.join(sde_connection_file, 'sweri.{}.intersections_source_list'.format(schema)))
-    fields = ['source', 'id_source', 'uid_fields', 'use_as_target', 'source_type', 'name']
-    with arcpy.da.SearchCursor(path.join(sde_connection_file, 'sweri.{}.intersections_source_list'.format(schema)),
-                               field_names=fields, sql_clause=(None, "ORDER BY source_type ASC")) as source_cursor:
-        for r in source_cursor:
-            s = {'source': r[0], 'id': r[2], 'source_type': r[4]}
-            if 'name' in domains and r[5] in domains['name']:
-                s['name'] = domains['name'][r[5]]
-            intersect_sources[r[1]] = s
-            if r[3] == 1:
-                intersect_targets[r[1]] = s
-    return intersect_sources, intersect_targets
-
-
-def update_schema_for_intersections_insert(intersect_result, fc_1_name, fc_2_name):
-    # update existing fields with new id column names
-    arcpy.management.AlterField(intersect_result, 'unique_id', 'id_1', 'id_1')
-    arcpy.management.AlterField(intersect_result, 'unique_id_1', 'id_2', 'id_2')
-    arcpy.management.AlterField(intersect_result, 'feat_source', 'id_1_source', 'id_1_source')
-    arcpy.management.AlterField(intersect_result, 'feat_source_1', 'id_2_source', 'id_2_source')
-    # add field names
-    arcpy.management.CalculateField(intersect_result, 'id_1_source', "'{}'".format(fc_1_name), 'PYTHON3')
-    arcpy.management.CalculateField(intersect_result, 'id_2_source', "'{}'".format(fc_2_name), 'PYTHON3')
-
-
-def fetch_domains(sde_connection_file, in_table):
-    """
-    fetches domains from a table
-    :param sde_connection_file: sde connection file path
-    :param in_table: table to fetch domains from
-    :return: dictionary of domains
-    """
-    all_domains = {d.name: d for d in arcpy.da.ListDomains(sde_connection_file)}
-    domain_dict = {
-        fld.name: {k: v for k, v in all_domains[fld.domain].codedValues.items()}
-        for fld in arcpy.ListFields(in_table) if fld.domain
-    }
-    return domain_dict
-
-
-def format_message(progress, buffer, label):
-    data = {
-        'progress': progress,
-        'buffer': buffer,
-        'label': label
-    }
-    return json.dumps(data)
-
+tools = r"C:\path\to\sweri_utils"
+sys.path.append(tools)
+from intersections import configure_intersection_sources, update_schema_for_intersections_insert
 
 if __name__ == '__main__':
     arcpy.env.overwriteOutput = True
     aoi = arcpy.GetParameterAsText(0) # AOI
-    schema='staging'
-    # schema = arcpy.GetParameterAsText(1)
-    sde_connection_file = 'Z:\\home\\arcgis\\sweri-staging\\sweri-data-tools\\sweri_staging.sde'
+    schema = arcpy.GetParameterAsText(1)
+    sde_connection_file = arcpy.GetParameterAsText(2)
     arcpy.AddMessage('Configuring Data Sources')
     treatment_intersections = path.join(sde_connection_file, 'sweri.{}.intersections'.format(schema))
     target_table = CreateTable(arcpy.env.scratchGDB, 'intersections', template=treatment_intersections)
