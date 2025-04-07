0ccd4da3277995b14430a0552e68494e9b858da0
Ben Holloway <bholloway@innovateteam.com>
2025-04-07 11:15:05 -0500

    updated logging

--- hosted_upload.py
+++ hosted_upload.py
@@ -89,8 +89,8 @@ def upload_chunk_to_feature_layer (feature_layer, schema, table, chunk_size, cur
 
     additions = feature_layer.edit_features(adds=features)
 
-    print(additions)
-    print(current_objectid)
+    logging.info(additions)
+    logging.info(current_objectid)
     return current_objectid
 
 def load_postgis_to_feature_layer(feature_layer, sqla_engine, schema, table, chunk_size, current_objectid):
@@ -118,7 +118,7 @@ def refresh_feature_data_and_swap_view_source(gis_con, view_id, source_feature_l
 
     new_source_feature_layer.manager.truncate()
 
-    print(f'beginning update for {new_source_item.name} from {schema}.{table}')
+    logging.info(f'beginning update for {new_source_item.name} from {schema}.{table}')
 
 
     load_postgis_to_feature_layer(new_source_feature_layer, sql_engine, schema, table, chunk_size, start_objectid)
@@ -128,7 +128,7 @@ def refresh_feature_data_and_swap_view_source(gis_con, view_id, source_feature_l
     new_source_item = gis.content.get(new_data_source_id)
     token = gis_con.session.auth.token
 
-    print(f'swapping {view_item.name} to data source {new_source_item.name}')
+    logging.info(f'swapping {view_item.name} to data source {new_source_item.name}')
     swizzle_service('https://gis.reshapewildfire.org/', view_item.name, new_source_item.name, token)
 
 if __name__ == '__main__':
