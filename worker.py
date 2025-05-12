c3d66378c03ad581a7f407a6db820a9815c21360
Keaton Shennan <kshennan@innovateteam.com>
2025-05-12 15:13:04 -0600

    update worker file

--- worker.py
+++ worker.py
@@ -3,23 +3,21 @@ import os
 import sys
 from os import path
 from kombu.utils.url import safequote
-sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
-aws_access_key = safequote(os.environ.get('CELERY_BROKER_AWS_ACCESS_KEY_ID'))
-aws_secret_key = safequote(os.environ.get('CELERY_BROKER_AWS_SECRET_ACCESS_KEY'))
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-broker_url = 'pyamqp://guest@rabbit:5672//'
-if os.environ.get('CELERY_BROKER_AWS_ACCESS_KEY_ID') and os.environ.get('CELERY_BROKER_AWS_SECRET_ACCESS_KEY'):
-    broker_url = "sqs://{aws_access_key}:{aws_secret_key}@".format(
-        aws_access_key=aws_access_key, aws_secret_key=aws_secret_key,
-    )
 app = Celery(
     'scripts',
-    broker=broker_url,
-    broker_transport_options={'region': os.environ.get('CELERY_BROKER_TRANSPORT_OPTIONS')} if os.environ.get('CELERY_BROKER_TRANSPORT_OPTIONS') else {},
-    result_backend=os.environ.get('CELERY_RESULT_BACKEND') if os.environ.get('CELERY_RESULT_BACKEND') else 'redis://redis:6379/0',
-    include=['intersections.tasks']
-)
+    broker="sqs://{aws_access_key}:{aws_secret_key}@".format(aws_access_key=safequote(os.environ.get(
+        'CELERY_BROKER_AWS_ACCESS_KEY_ID')),
+        aws_secret_key=safequote(os.environ.get(
+            'CELERY_BROKER_AWS_SECRET_ACCESS_KEY')) )if os.environ.get(
+            'CELERY_BROKER_AWS_ACCESS_KEY_ID') and os.environ.get(
+            'CELERY_BROKER_AWS_SECRET_ACCESS_KEY') else 'pyamqp://guest@rabbit:5672//',
+        broker_transport_options={'region': os.environ.get('CELERY_BROKER_TRANSPORT_OPTIONS')} if os.environ.get(
+            'CELERY_BROKER_TRANSPORT_OPTIONS') else {},
+        result_backend=os.environ.get('CELERY_RESULT_BACKEND') if os.environ.get(
+            'CELERY_RESULT_BACKEND') else 'redis://redis:6379/0',
+        include=['intersections.tasks']
+    )
 # todo: will need to update region for sqs .... see https://docs.celeryq.dev/en/stable/getting-started/backends-and-brokers/sqs.html
-
-
