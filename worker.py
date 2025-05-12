248635413f3a8138f26fe63789bea6a0a2426ad3
Keaton Shennan <kshennan@innovateteam.com>
2025-05-12 10:57:10 -0600

    update sqs celery broker setting

--- worker.py
+++ worker.py
@@ -2,10 +2,20 @@ from celery import Celery
 import os
 import sys
 from os import path
+from kombu.utils.url import safequote
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
+aws_access_key = safequote(os.environ.get('CELERY_BROKER_AWS_ACCESS_KEY_ID'))
+aws_secret_key = safequote(os.environ.get('CELERY_BROKER_AWS_SECRET_ACCESS_KEY'))
+
+broker_url = 'pyamqp://guest@rabbit:5672//'
+if os.environ.get('CELERY_BROKER_AWS_ACCESS_KEY_ID') and os.environ.get('CELERY_BROKER_AWS_SECRET_ACCESS_KEY'):
+    broker_url = "sqs://{aws_access_key}:{aws_secret_key}@".format(
+        aws_access_key=aws_access_key, aws_secret_key=aws_secret_key,
+    )
 app = Celery(
     'scripts',
-    broker=os.environ.get('CELERY_BROKER_URL', 'pyamqp://guest@rabbit:5672//'),
+    broker=broker_url,
     broker_transport_options={'region': os.environ.get('CELERY_BROKER_TRANSPORT_OPTIONS')} if os.environ.get('CELERY_BROKER_TRANSPORT_OPTIONS') else {},
     result_backend=os.environ.get('CELERY_RESULT_BACKEND') if os.environ.get('CELERY_RESULT_BACKEND') else 'redis://redis:6379/0',
     include=['intersections.tasks']
