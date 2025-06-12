3a9923f7735b6f641f79af1ab4d288dbed058711
Keaton Shennan <kshennan@innovateteam.com>
2025-06-12 10:15:33 -0600

    update connect from envs in celery task

--- intersections/utils.py
+++ intersections/utils.py
@@ -4,12 +4,12 @@ import os
 from sweri_utils.sql import connect_to_pg_db
 import logging
 
-def create_db_conn_from_envs(k):
-    docker_db_host = os.getenv(f'{k}_DB_HOST')
-    docker_db_port = int(os.getenv(f'{k}_DB_PORT'))
-    docker_db_name = os.getenv(f'{k}_DB_NAME')
-    docker_db_user = os.getenv(f'{k}_DB_USER')
-    docker_db_password = os.getenv(f'{k}_DB_PASSWORD')
+def create_db_conn_from_envs():
+    docker_db_host = os.getenv('DB_HOST')
+    docker_db_port = int(os.getenv('DB_PORT'))
+    docker_db_name = os.getenv('DB_NAME')
+    docker_db_user = os.getenv('DB_USER')
+    docker_db_password = os.getenv('DB_PASSWORD')
     return connect_to_pg_db(docker_db_host, docker_db_port, docker_db_name, docker_db_user, docker_db_password)
 
 
