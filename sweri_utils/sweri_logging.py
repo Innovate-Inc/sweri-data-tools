089f07be937afafc88687c012dd0bc72fa3fecc5
Travis <tbock@innovateteam.com>
2025-09-24 09:20:31 -0700

    watchtower/creds for logging not always available

--- sweri_utils/sweri_logging.py
+++ sweri_utils/sweri_logging.py
@@ -1,11 +1,15 @@
 import logging
-import watchtower
 import sys
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', encoding='utf-8',
                     level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S', force=True)
-logger.addHandler(watchtower.CloudWatchLogHandler())
+try:
+    import watchtower
+    logger.addHandler(watchtower.CloudWatchLogHandler())
+except Exception as e:
+    logger.warning(f'watchtower not available: {e}')
+
 stream_handler = logging.StreamHandler(sys.stdout)
 logger.addHandler(stream_handler)
 def log_this(func):
