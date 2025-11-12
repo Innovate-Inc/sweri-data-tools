9c8c787648669d4b11caf28e76105d0d10a40532
Ben Holloway <bholloway@innovateteam.com>
2025-11-12 09:47:26 -0600

    commented out watchtower from logging.

--- sweri_utils/sweri_logging.py
+++ sweri_utils/sweri_logging.py
@@ -4,11 +4,11 @@ import sys
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', encoding='utf-8',
                     level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S', force=True)
-try:
-    import watchtower
-    logger.addHandler(watchtower.CloudWatchLogHandler())
-except Exception as e:
-    logger.warning(f'watchtower not available: {e}')
+# try:
+#     import watchtower
+#     logger.addHandler(watchtower.CloudWatchLogHandler())
+# except Exception as e:
+#     logger.warning(f'watchtower not available: {e}')
 
 stream_handler = logging.StreamHandler(sys.stdout)
 logger.addHandler(stream_handler)
