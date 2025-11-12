37e434b6b0beb50d0714ace18aa69b5752db49b5
Ben Holloway <bholloway@innovateteam.com>
2025-11-12 10:22:25 -0600

    Moving inside functions.

--- sweri_utils/tests.py
+++ sweri_utils/tests.py
@@ -2,10 +2,9 @@ import os
 from unittest import TestCase
 from unittest.mock import patch, Mock, call, mock_open, MagicMock
 from . import download, files, conversion, s3, sql
-from .hosted import verify_feature_count
 from .swizzle import get_layer_definition, get_new_definition, get_view_admin_url, clear_current_definition, \
     add_to_definition, swizzle_service
-
+from .hosted import verify_feature_count
 
 class DownloadTests(TestCase):
     def setUp(self):
@@ -1058,8 +1057,8 @@ class SwizzleTests(TestCase):
         with self.assertRaises(TypeError):
             swizzle_service('http://example.com', 'view_name', 'new_service_name', 'invalid_token')
 
-@patch.dict('sys.modules', {'worker': Mock(app=Mock())})
 class HostedTests(TestCase):
+    @patch.dict('sys.modules', {'worker': Mock(app=Mock())})
     @patch('sweri_utils.hosted.get_count')
     def test_verify_feature_same_count(self, mock_get_count):
         mock_conn = MagicMock()
@@ -1069,6 +1068,7 @@ class HostedTests(TestCase):
 
         verify_feature_count(mock_conn, 'schema', 'table', mock_fl)
 
+    @patch.dict('sys.modules', {'worker': Mock(app=Mock())})
     @patch('sweri_utils.hosted.get_count')
     def test_verify_feature_mismatched_count(self, mock_get_count):
         mock_conn = MagicMock()
